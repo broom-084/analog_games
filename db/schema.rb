ee59d22940f1fbd3c60659800912f23281c852b1
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-03-23 15:02:20 +0000

    ポストコントローラー

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_04_145401) do
+ActiveRecord::Schema.define(version: 2024_03_23_141750) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
