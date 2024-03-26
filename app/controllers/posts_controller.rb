0387c600e5a9718bb0c3ed7ccfd38ac1e361b933
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-03-26 15:18:01 +0000

    ブートストラップ導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,63 @@
 class PostsController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
+    @user = current_user
+    @Posts = Post.all
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "You have created post successfully."
+      redirect_to post_path(@post.id)
+    else
+      @user = current_user
+      @posts = Post.all
+      render :index
+    end
+  end
+  
+  def edit
+    @user = current_user
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "You have updated post successfully."
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
   
   def show
-  end  
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @post_new = Post.new
+  end
   
-  def edit
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Post was successfully destroyed."
+    redirect_to posts_path
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :review)
+  end
+  
+  def is_matching_login_user
+    post = Post.find(params[:id])
+    unless post.user_id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
