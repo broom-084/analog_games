bd236c9e7ab24fd06175324a78dd1a51af5322cd
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-03-27 15:11:24 +0000

    バリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :review, presence: true, length: {maximum: 200}
 end
