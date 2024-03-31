370d3b05b924544f1e6312f4ca7d12fcf08a2ba5
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-03-31 16:13:28 +0000

    エラー発生中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
+  def show
+    @user = User.find(params[:id])
+    @post_new = Post.new
+    @post = @user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+  def index
+    @user = current_user
+    @users = User.all
+    @post_new = Post.new
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :review)
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+end
+
---
+++ db/migrate/20240331150833_add_introductionto_users.rb
@@ -0,0 +1,5 @@
+class AddIntroductiontoUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :introduction, :string
+  end
+end
