0eab3432c0985f5397ff5c53743a356e27c07892
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-04-16 04:17:14 +0000

    検索更新

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post = Post.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post = Post.where("title LIKE?","%#{word}%")
+    else
+      @post = Post.all
+    end
+  end
+  
   validates :title, presence: true
   validates :review, presence: true, length: {maximum: 200}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,20 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+  
   validates :name, length: {in: 2..20}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
