893a649758ac4cf05dcb8f6b2ae824114f33ba41
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2024-04-16 08:48:34 +0000

    管理更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,22 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
+  
   get 'users/index'
   get 'users/edit'
   get 'users/show'
